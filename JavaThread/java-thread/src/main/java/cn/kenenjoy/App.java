919a028ccb79a08bdcaaa31e0f5398b9bfa405ce
何发 <207324509@qq.com>
2017-11-22 21:35:29 +0800

    创建Java线程项目

---
+++ JavaThread/java-thread/src/main/java/cn/kenenjoy/App.java
@@ -0,0 +1,13 @@
+package cn.kenenjoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
