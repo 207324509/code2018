42a06fb7e2aefe3d714088f739c1fcec4e296a6c
何发 <207324509@qq.com>
2017-12-08 11:10:16 +0800

    修改代码注释

--- java-thread/src/main/java/cn/kenenjoy/v3/PoolThreadSimple.java
+++ java-thread/src/main/java/cn/kenenjoy/v3/PoolThreadSimple.java
@@ -17,7 +17,7 @@ public class PoolThreadSimple {
     public static void main(String[] args) {
         /**
          * corePoolSize：核心大小，线程池初始化的时候，就会有这么大
-         * maximumPoolSize：线程池最大线程数
+         * maximumPoolSize：线程池最大线程数，corePoolSize参数和maximumPoolSize参数一致时，keepAliveTime和TimeUnit便失去了意义。
          * keepAliveTime：如果当前线程池中线程数大于corePoolSize，多余的线程在等待keepAliveTime后没有任务，它就会被回收。
          * TimeUnit：等待时间keepAliveTimed的单位
          * workQueue：等待队列。
@@ -25,7 +25,7 @@ public class PoolThreadSimple {
         ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2, 10, 1, TimeUnit.MINUTES, new SynchronousQueue<Runnable>());
 
         // 不常用设置
-//        poolExecutor.allowCoreThreadTimeOut(true );// 在等待keepAliveTime时间后全部进行回收，包括"核心线程"在内
+//        poolExecutor.allowCoreThreadTimeOut(true);// 在等待keepAliveTime时间后全部进行回收，包括"核心线程"在内
 //        poolExecutor.prestartAllCoreThreads();// 在线程池创建时，还没有收到任何任务的情况下可以先创建符合corePoolSize参数值的线程数
 
         for (int index = 0; index < 10; index++) {
