d4297fca7bec60355a0b52efc869eceaef5ee423
何发 <207324509@qq.com>
2018-05-01 10:42:12 +0800

    熔断监控Hystrix Dashboard和Turbine示例

--- spring-cloud-consumer-hystrix-dashboard/src/main/java/cn/kenenjoy/ConsumerHystrixApplication.java
+++ spring-cloud-consumer-hystrix-dashboard/src/main/java/cn/kenenjoy/ConsumerHystrixApplication.java
@@ -8,18 +8,23 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * Created by hefa on 2018/4/30.
  *
  * @EnableDiscoveryClient :启用服务注册与发现
  * @EnableFeignClients：启用feign进行远程调用
+ * @EnableHystrixDashboard：启用Hystrix Dashboard
+ * @EnableCircuitBreaker：启用熔断器
+ * @EnableTurbine：激活对Turbine的支持
  */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableTurbine
 public class ConsumerHystrixApplication {
     private static final Logger logger = LoggerFactory.getLogger(ConsumerHystrixApplication.class);
 
