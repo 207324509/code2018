66d15ae036f8d398a0dfca2fc7ccfba1186c5cad
何发 <207324509@qq.com>
2018-05-24 22:27:21 +0800

    spring-cloud-config-server示例

---
+++ spring-cloud-config-server/src/main/java/cn/kenenjoy/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package cn.kenenjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
