95be01f6c6e1d07146bb84c48609b2ea4638c142
何发 <hefa@wondersgroup.com>
2019-04-28 11:44:20 +0800

    spring config 集成svn

--- spring-cloud-config-client/src/main/java/cn/kenenjoy/HelloController.java
+++ spring-cloud-config-client/src/main/java/cn/kenenjoy/HelloController.java
@@ -1,19 +1,24 @@
 package cn.kenenjoy;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by hefa on 2018/5/24.
  */
+@RefreshScope
 @RestController
 public class HelloController {
     @Value("${app.name}")
     private String name;
 
+    @Value("${app.version}")
+    private String version;
+
     @RequestMapping("/name")
     public String getName() {
-        return this.name;
+        return this.name + ":" + this.version;
     }
 }
